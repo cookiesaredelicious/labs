541b91558328263e7cfabd2138d60dcb02c4d8cd
Jimmy Yau <personal@takkan.org>
2025-03-09 16:33:10 +1100

    first attempt

--- labs/lab03/custom_cake.c
+++ labs/lab03/custom_cake.c
@@ -8,11 +8,28 @@
 #include <stdio.h>
 
 
-int main(void){
+int main(void) {
 	
 	int layers;
-	printf("Enter layers: ");
+	
+	printf("How many layers: ");
 	scanf("%d", &layers);
 	
-	return 0;
+	char input[layers + 1];
+	
+	printf("Please enter layers: ");
+	scanf("%s", input);
+	
+	
+	for (int i = 0; i < layers; i++) {
+		
+		for (int x = 0; x < layers; x++) {
+			
+			printf("%c", input[i]);
+		}
+		
+		printf("\n");
+		
+		return 0;
+	}
 }
