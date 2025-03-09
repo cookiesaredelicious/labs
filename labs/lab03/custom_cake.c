1225ac43161a578d2cd68fc73d43352c6d43dd04
Jimmy Yau <personal@takkan.org>
2025-03-09 16:05:37 +1100

    created lab03 folder

---
+++ labs/lab03/custom_cake.c
@@ -0,0 +1,18 @@
+//
+//  custom_cake.c
+//
+//
+//  Created by Jimmy Yau on 9/3/2025.
+//
+
+#include <stdio.h>
+
+
+int main(void){
+	
+	int layers;
+	printf("Enter layers: ");
+	scanf("%d", &layers);
+	
+	return 0;
+}
