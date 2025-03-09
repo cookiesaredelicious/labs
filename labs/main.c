73357f91a54a3a4782f0867908023013da4db061
Jimmy Yau <personal@takkan.org>
2025-03-09 15:51:33 +1100

    Initial Commit

---
+++ labs/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  labs
+//
+//  Created by Jimmy Yau on 9/3/2025.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	printf("Hello, World!\n");
+	return 0;
+}
